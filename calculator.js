cfd5d5eaf667513b7aca9a39635a677bbdea6b40
mahiagrawal21 <138103880+mahiagrawal21@users.noreply.github.com>
2023-07-06 13:39:43 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.get('/',(req, res)=> {
 
-res.send("kuch bhi");
+res.send("kuch bhi"); // for sending
 })
 
 app.post('/calculate', (req, res)=>{
